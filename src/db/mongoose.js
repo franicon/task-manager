124f9649547c509f3755db6e7b134088cca2689d
Abayomi Aluko <franicons@gmail.com>
2023-07-17 15:10:59 +0100

    feat: development * installing Express, and setup package.json

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,54 +1,9 @@
 const mongoose = require('mongoose');
-const {Schema} = require('mongoose');
-const validator = require('validator');
+const { Schema } = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27016/task-manager-api')
 
-// const User = mongoose.model('User', new Schema({
-//     name: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     password: {
-//         type: String,
-//         minLength: 7,
-//         trim: true,
-//         validate(value) {
-//             if (value.includes('password')) {
-//                 throw new Error('you can not use the word password')
-//             }
-//         }
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         lowercase: true,
-//         validate(value) {
-//             if (!validator.isEmail(value)) {
-//                 throw new Error('Email is invalid')
-//             }
-//         }
-//     },
-//     age: {
-//         type: Number,
-//         default: 0,
-//         validate(value) {
-//             if (value < 0) {
-//                 throw new Error('age must be a positive number')
-//             }
-//         }
-//     }
-// }))
 
-// const me = new User({name: 'Mike', email: ' mike@Gmail.Com ', password:'!icon++'})
-//
-// me.save().then((me) => {
-//     console.log('User', me)
-// }).catch((err) => {
-//     console.log('error', err)
-// }).finally(() => mongoose.disconnect())
 
 const Task = mongoose.model('Task', new Schema({
     description: {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
-const index = express()
+const app = express()
 const PORT = process.env.PORT || 3000
 
-index.listen(PORT, () => {
-    console.log('server started on port ' + PORT)
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    res.send('testing endpoints')
 })
+
+
+app.listen(PORT, () => {
+    console.log('server started on port ' + PORT)
+});
