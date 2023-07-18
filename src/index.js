79545d601f6bd580342c5089975a520f2cb970e8
Abayomi Aluko <franicons@gmail.com>
2023-07-18 20:43:04 +0100

    feat: development

--- src/index.js
+++ src/index.js
@@ -1,79 +1,21 @@
 const express = require('express');
 
 require('./db/mongoose');
+
+// Models
 const User = require('./models/user');
 const Task = require('./models/task');
+
+// Router
+const userRouter = require('./routers/user')
 
 const app = express();
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
+app.use(userRouter)
 
 // User
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send('User not found')
-        }
-        res.status(200).send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValid = updates.every((item) => allowedUpdates.includes(item))
-
-    if (!isValid) {
-        return res.status(400).send({error: "Invalid updates"})
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!user) {
-            return res.status(404).send('User not found')
-        }
-        res.status(200).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            return res.status(404).send({error: 'user not found'})
-        }
-        res.status(200).send({msg: 'Deleted successfully'})
-    }catch (e) {
-        res.status(400).send(e)
-    }
-})
 
 // Task
 app.post('/tasks', async (req, res) => {
