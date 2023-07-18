54fbe765244a848be270cf593066041051ac8fff
Abayomi Aluko <franicons@gmail.com>
2023-07-18 20:33:58 +0100

    feat: development

--- src/index.js
+++ src/index.js
@@ -131,6 +131,18 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send({error: 'No task could be found with the ID'})
+        }
+        res.status(200).send({msg: 'Task Deleted Successfully'})
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.listen(PORT, () => {
     console.log('server started on port ' + PORT)
 });
