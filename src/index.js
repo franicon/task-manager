40b8317442d1dd5af5df4d0d9cc24dc9045f2963
Abayomi Aluko <franicons@gmail.com>
2023-07-19 16:07:52 +0100

    feat: development

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 require('./db/mongoose');
 
-// Router
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
@@ -10,7 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
-
 app.use(userRouter);
 app.use(taskRouter);
 
