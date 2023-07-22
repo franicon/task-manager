e50a3c2c00d1ac88e87b632662e05cc2dc6e1014
Abayomi Aluko <franicons@gmail.com>
2023-07-22 20:18:08 +0100

    feat: development

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,6 @@
 const multer = require('multer');
 const express = require('express');
 const avatar = multer({
-    dest: 'avatar',
     limits: {
         fileSize: 1000000
     },
@@ -109,10 +108,23 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
-router.post('/users/me/avatar', auth, avatar.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, avatar.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.status(200).send()
-},(error, req, res, next) => {
+}, (error, req, res, next) => {
     res.status(400).send({error: error.message})
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    try {
+        req.user.avatar = undefined
+        await req.user.save()
+        res.status(200).send(req.user)
+    } catch (err) {
+        console.log(err)
+        res.status(400).send(err)
+    }
 })
 
 module.exports = router
